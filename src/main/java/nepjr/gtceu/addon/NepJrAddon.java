9cdb8b4a5a8fc47c17afa07482814fa6282a908b
Gudak0 <gudak0@protonmail.com>
2022-11-05 23:10:15 -0700

    minor spelling mistake fixed

--- src/main/java/nepjr/gtceu/addon/NepJrAddon.java
+++ src/main/java/nepjr/gtceu/addon/NepJrAddon.java
@@ -18,7 +18,7 @@ public class NepJrAddon
 {
     public static final String MODID = "gregious-maximus";
     public static final String NAME = "Gregious Maxmimus";
-    public static final String VERSION = "1.1 Beta 1";
+    public static final String VERSION = "1.1-b1";
 
     private static Logger logger;
 
